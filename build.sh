07af3fa2d4f1823ca9d439d0fd03071da6824c3b
tosh7 <zlia.6.lj.425@gmail.com>
2023-03-22 18:01:16 +0900

    Add clauclator

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ rm -rf build
 mkdir build
 mkdir temporary
 clang++ -o temporary/main.o -c source/main.cpp
-ar rcs build/libmoai.a temporary/main.o
+clang++ -o temporary/calculator.o -c source/calculator.cpp
+ar rcs build/libmoai.a temporary/main.o temporary/calculator.o
 rm -rf temporary
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "calculator.h"
 
 using namespace std;
 
 int main(void) {
     cout << "Hello world!" << endl;
+    sum(2, 3);
     return 0;
 }
