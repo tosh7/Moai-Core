a2071b01b3857d09ceeb1be6285a41496985b20b
tosh7 <zlia.6.lj.425@gmail.com>
2023-03-25 19:52:14 +0900

    Modified build file to create ios targeted binary

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 rm -rf build
 mkdir build
 mkdir temporary
-clang++ -o temporary/main.o -c source/main.cpp
-clang++ -o temporary/calculator.o -c source/calculator.cpp
+
+clang++ -std=c++17 \
+    -target arm64-apple-ios13.0 \
+    -isysroot $(xcrun --sdk iphoneos --show-sdk-path) \
+    -o temporary/main.o -c source/main.cpp
+
+clang++ -std=c++17 \
+    -target arm64-apple-ios13.0 \
+    -isysroot $(xcrun --sdk iphoneos --show-sdk-path) \
+    -o temporary/calculator.o -c source/calculator.cpp
+    
 ar rcs build/libmoai.a temporary/main.o temporary/calculator.o
 rm -rf temporary
