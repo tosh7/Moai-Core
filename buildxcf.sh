ce6bb4f3299a366ed2c0a12c178bcbac26f4a441
tosh7 <zlia.6.lj.425@gmail.com>
2023-03-25 19:30:32 +0900

    Successed with creating XCFramework

--- buildxcf.sh
+++ buildxcf.sh
@@ -8,7 +8,6 @@ cp build/libmoai.a moai-x86_64
 
 xcodebuild -create-xcframework \
     -library moai-arm64/libmoai.a \
-    -library moai-x86_64/libmoai.a \
     -output build/moai.xcframework
 
 rm -rf moai-arm64
