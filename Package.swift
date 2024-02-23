ca490724f73058c719d3faade1dc784db5e95691
tosh7 <zlia.6.lj.425@gmail.com>
2024-02-23 18:37:50 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "MoaiCore",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MoaiCore",
-            targets: ["MoaiCore"]),
+        .library(name: "MoaiCore", targets: ["MoaiCore"]),
     ],
     platforms: [
         .iOS(.v15),
@@ -21,8 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MoaiCore"),
-            sources: [souece/*.h, souece.m],
-        )
+            name: "MoaiCore",
+            path: "source",
+        ),
     ],
 )
