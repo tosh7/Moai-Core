d5d56132d37496eb4b1bd19ceab951f79712f028
tosh7 <zlia.6.lj.425@gmail.com>
2026-08-14 23:35:33 +0900

    Keep calculator after all
    Restore sum() and its header. Only main.cpp, the Hello world entry
    point, was meant to go.

---
+++ source/calculator.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "calculator.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
---
+++ source/include/calculator.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int sum(int a, int b);
+
+#endif
